18d87d792d9003f1931b223dcf16ad111a3b0e1b
Jaeyeon Kim <0310kjy@gmail.com>
2025-01-08 11:50:35 +0000

    add salmonn efficiency evaluate codes

---
+++ salmonn_utils.py
@@ -0,0 +1,20 @@
+import sys
+from pathlib import Path
+
+# Add custom module path
+sys.path.append(str(Path(__file__).parent / "np-app-audiolm-trainer"))
+
+# Custom modules
+from models.salmonn import SALMONN
+
+def load_preprocessor(cfg):
+    salmonn_preprocessor = SALMONN.from_config(cfg.config.model)
+    salmonn_preprocessor.to(cfg.config.run.device)
+    salmonn_preprocessor.eval()
+    return salmonn_preprocessor
+
+
+def load_model(salmonn_preprocessor):
+    model = salmonn_preprocessor.llama_model
+    tokenizer = salmonn_preprocessor.llama_tokenizer
+    return model, tokenizer
