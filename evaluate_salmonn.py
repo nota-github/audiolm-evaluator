7418bb60ef97cb4c6a91a8f34eb2f75fa73da44e
seonguk.jo <seonguk.jo@nota.ai>
2025-01-14 20:32:09 +0900

    refactor new arg, add datetime when making submission

--- evaluate_salmonn.py
+++ evaluate_salmonn.py
@@ -6,7 +6,8 @@ import torch
 import pandas as pd
 from pathlib import Path
 from tqdm import tqdm
-
+import os
+import time
 # Add custom module path
 sys.path.append(str(Path(__file__).parent / "audiolm-trainer"))
 
@@ -33,10 +34,47 @@ def parse_args():
         "in xxx=yyy format will be merged into config file (deprecate), "
         "change to --cfg-options instead.",
     )
-    parser.add_argument("--task", type=str, default=None, help="Task to evaluate", choices=['asr', 'aac'])
-    parser.add_argument("--skip_scoring", action='store_true', help='if True, skip scoring after inference')
-    return parser.parse_args()
+    # --- Deprecated options ---
+    parser.add_argument("--task", type=str, default=None, 
+                    help="(Deprecate) Task to evaluate. Use --mode instead. This option will be removed in a future version.", 
+                    choices=['asr', 'aac'])
 
+    parser.add_argument("--skip_scoring", action='store_false', default=True, 
+                    help="(Deprecate) If True, skip scoring after inference. Use --mode instead. This option will be removed in a future version.")
+    # --- Deprecated options end ---
+    # --- New options ---
+    parser.add_argument("--mode", type=str, default="valid_aac", 
+                    help="Mode to evaluate. Supports submission and validation modes for ASR and AAC tasks.", 
+                    choices=['submission_asr', 'submission_aac', 'valid_asr', 'valid_aac'])
+    # --- New options end ---
+
+    args = parser.parse_args()
+
+    if args.mode is None:
+        # --- For Previous Version ---
+        if args.task is None:
+            raise ValueError("Either --task or --mode must be provided")
+        args.mode = convert_task_to_mode(args.task, args.skip_scoring)
+
+    # --- Override Previous Version Args ---
+    args.task = args.mode.split("_")[1]
+    args.make_submission = args.mode.split("_")[0] == "submission"
+
+    return args
+
+def convert_task_to_mode(task, skip_scoring):
+    if skip_scoring:
+        if task == 'asr':
+            return 'submission_asr'
+        elif task == 'aac':
+            return 'submission_aac'
+    else:
+        if task == 'asr':       
+            return 'valid_asr'
+        elif task == 'aac':
+            return 'valid_aac'
+    
+    raise ValueError(f"Invalid task: {task} | {skip_scoring}")
 
 def get_dataset(dataset_cfg, run_cfg, task):
     testset = SALMONNTestDataset(
@@ -46,10 +84,17 @@ def get_dataset(dataset_cfg, run_cfg, task):
     test_loader = get_dataloader(testset, run_cfg, is_train=False, use_distributed=False)
     return test_loader
 
+def replace_test_ann_path(cfg):
+    if "test_ann_path" not in cfg.config.datasets.keys():
+        if args.task == "asr":
+            cfg.config.datasets.test_ann_path = cfg.config.datasets.test_ann_path_asr
+        elif args.task == "aac":
+            cfg.config.datasets.test_ann_path = cfg.config.datasets.test_ann_path_aac
+    return cfg
 
 def main(args):
     cfg = Config(args)
-    
+    cfg = replace_test_ann_path(cfg)
     # Load models
     salmonn_preprocessor = load_preprocessor(cfg)
     llama_model, tokenizer = load_model(salmonn_preprocessor)
@@ -113,20 +158,26 @@ def main(args):
         hyp = [result.split(generate_cfg.end_sym)[0].lower() for result in results]
         hyps.extend(hyp)
 
-        if not args.skip_scoring:
+        if not args.make_submission:
             ref = samples["text"]
             refs.extend(ref)
 
-    if not args.skip_scoring:
+    
+
+    if args.make_submission:
+        os.makedirs("submission_results", exist_ok=True)
+        file_name = f"submission_results/{time.strftime('%Y-%m-%d_%H-%M-%S')}_{args.mode}.csv"
+    else:
         if args.task == 'asr':
             compute_wer(hyps, refs)
             
         elif args.task == 'aac':
             compute_spider(hyps, refs)
+        os.makedirs("valid_results", exist_ok=True)
+        file_name = f"valid_results/{time.strftime('%Y-%m-%d_%H-%M-%S')}_{args.mode}.csv"
 
     result_df = pd.DataFrame({"testset_id": testset_ids, "text": hyps})
-    result_df.to_csv("submission.csv", index=False)
-
+    result_df.to_csv(file_name, index=False)
 
 if __name__ == '__main__':
     args = parse_args()
